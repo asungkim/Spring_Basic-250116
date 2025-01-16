5f4af854d3e5e68bc8d8bc17ff62a333e940c6d3
asungkim <asungkim007@naver.com>
2025-01-16 14:22:57 +0900

    modify

--- src/main/java/com/example/demo/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/example/demo/domain/wiseSaying/entity/WiseSaying.java
@@ -2,12 +2,15 @@ package com.example.demo.domain.wiseSaying.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
 public class WiseSaying {
     private int id;
+    @Setter
     private String content;
+    @Setter
     private String author;
 
 
