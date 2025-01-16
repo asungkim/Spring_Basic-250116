abce7cd3fbfe32f9808f31a4b0583b114316fba1
asungkim <kimasung143@gmail.com>
2025-01-16 14:22:57 +0900

    modify

--- src/main/java/com/example/demo/domain/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/example/demo/domain/wiseSaying/service/WiseSayingService.java
@@ -57,4 +57,18 @@ public class WiseSayingService {
     public boolean deleteById(int id) {
         return wiseSayingList.removeIf(w->w.getId()==id);
     }
+
+    public WiseSaying modify(int id, String content, String author) {
+        Optional<WiseSaying> opWiseSaying = getItem(id);
+
+        if (opWiseSaying.isEmpty()) {
+            throw new IllegalArgumentException("해당 id의 명언이 없습니다.");
+        }
+
+        WiseSaying wiseSaying = opWiseSaying.get();
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+
+        return wiseSaying;
+    }
 }
