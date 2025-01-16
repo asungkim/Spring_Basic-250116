8fe0fa424a94d46821f8f4f3f36cbaa9ddf0ec14
asungkim <kimasung143@gmail.com>
2025-01-16 11:30:11 +0900

    ariticleList

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -11,92 +11,79 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@ResponseBody
 public class HomeController {
 
     private int age = 0;
 
 
     @GetMapping("/")
-    @ResponseBody // 메서드에 리턴값을 브라우저에 보여준다
     public String home() {
         return "hello";
     }
 
     @GetMapping("/introduce")
-    @ResponseBody
     public String introduce() {
         return "안녕하세요 저는 홍길동입니다.";
     }
 
     @GetMapping("/ageUp")
-    @ResponseBody
     public int ageUp() {
         age++;
         return age;
     }
 
     @GetMapping("/byte")
-    @ResponseBody
     public byte getByte() {
         return 1;
     }
 
     @GetMapping("/short")
-    @ResponseBody
     public short getShort() {
         return 1;
     }
 
     @GetMapping("/long")
-    @ResponseBody
     public long getLong() {
         return 1;
     }
 
     @GetMapping("/float")
-    @ResponseBody
     public float getFloat() {
         return 3.14f;
     }
 
     @GetMapping("/double")
-    @ResponseBody
     public double getDouble() {
         return 3.14;
     }
 
     @GetMapping("/char")
-    @ResponseBody
     public char getChar() {
         return '안';
     }
 
     @GetMapping("/boolean")
-    @ResponseBody
     public boolean getBoolean() {
         return true;
     }
 
     @GetMapping("/arr")
-    @ResponseBody
     public String[] getArr() {
         return new String[]{"a", "b", "c"};
     }
 
     @GetMapping("/list")
-    @ResponseBody
     public List<String> getList() {
         return List.of("a", "b", "c");
     }
 
     @GetMapping("/map")
-    @ResponseBody
     public Map<String, String> getMap() {
         return Map.of("a", "b", "c", "d");
     }
 
     @GetMapping("/article")
-    @ResponseBody
     public Article getArticle() {
         return Article.builder()
                 .body("내용")
@@ -104,6 +91,24 @@ public class HomeController {
                 .deleted(true)
                 .build();
     }
+
+    @GetMapping("/mapList")
+    public List<Map<String, String>> getMapList() {
+        return List.of(
+                Map.of("k1", "v1", "k2", "v2"),
+                Map.of("k3", "v3", "k4", "v4")
+        );
+    }
+
+    @GetMapping("/articleList")
+    public List<Article> getArticleList() {
+        return List.of(
+                Article.builder().title("제목1").body("내용1").deleted(false).build(),
+                Article.builder().title("제목2").body("내용2").deleted(false).build()
+        );
+    }
+
+
 
 }
 
